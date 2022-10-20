7478d13fd446b8414d3fb0aae414fad7bd0a91d2
Eduardo Romero <eduardoromeror9@outlook.com>
2022-10-20 06:11:38 -0300

    crear un mixin para media queries

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 
 function css(done) {
 
-  src('src/scss/app.scss') // Identify the file sass
+  src('src/scss/**/*.scss') // Identify the file sass
+    .pipe(plumber()) 
     .pipe( sass() ) // Compile the sass file
     .pipe( dest('build/css') ); // Identify the destination folder
-
-
   done(); //this is the callback function that tells gulp that the task is complete
 }
 
+function dev(done) {
+  watch('src/scss/**/*.scss', css); // Watch the sass files and compile them
+  done();
+  console.log('Waiting changes, compiling SASS... For exit press CTRL+C');
+}
+
 exports.css = css;
+exports.dev = dev;
